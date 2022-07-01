5b633bb54c5893aeb50d4c43e0cf42889ea3a814
Hermes <hermeschigoziri@gmail.com>
2022-07-01 14:26:13 +0100

    add shuffling logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Header from './components/Header/Header';
 import './App.css';
 import Scoreboard from './components/Scoreboard/Scoreboard';
@@ -16,7 +17,85 @@ import Hinata from "./resources/images/hinata.png";
 import Sakura from "./resources/images/sakura.png";
 import Ino from "./resources/images/ino.png";
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function shuffleArray(array) {
+  let copiedArr = [...array]
+  for (let i = copiedArr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copiedArr[i], copiedArr[j]] = [copiedArr[j], copiedArr[i]];
+  }
+  return copiedArr;
+}
+
 function App() {
+  const [cardItems, setCardItems] = useState([
+    {
+      alt : "Naruto",
+      src : Naruto
+    },
+    {
+      alt : "Sasuke",
+      src : Sasuke
+    },
+    {
+      alt : "Sakura",
+      src : Sakura
+    },
+    {
+      alt : "Gaara",
+      src : Gaara
+    },
+    {
+      alt : "Kakashi",
+      src : Kakashi
+    },
+    {
+      alt : "Itachi",
+      src : Itachi
+    },
+    {
+      alt : "Jiraiya",
+      src : Jiraiya
+    },
+    {
+      alt : "Tsunade",
+      src : Tsunade
+    },
+    {
+      alt : "Shikamaru",
+      src : Shikamaru
+    },
+    {
+      alt : "Choji",
+      src : Choji
+    },
+    {
+      alt : "Hinata",
+      src : Hinata
+    },
+    {
+      alt : "Ino",
+      src : Ino
+    }
+  ]);
+
+  const shuffleCardItems = () => {
+    setCardItems(latest => {
+      return shuffleArray(latest);
+    });
+  }
+
+  const jsxCardItems = cardItems.map(item => {
+    return (
+      <CardItem key={item.alt} img={item.src} alt={item.alt} onClick={shuffleCardItems} /> 
+    );
+  });
+
   return (
     <>
       <Header />
@@ -24,7 +103,8 @@ function App() {
         <span className='info-text'>Pick all individual cards with no repitition</span>
         <Scoreboard current={0} highest={0} />
         <div className='board'>
-          <CardItem img={Naruto} imgAlt={"Naruto"} />
+        {jsxCardItems}
+          {/* <CardItem img={Naruto} imgAlt={"Naruto"} />
           <CardItem img={Sasuke} imgAlt={"Sasuke"} />
           <CardItem img={Itachi} imgAlt={"Itachi"} />
           <CardItem img={Sakura} imgAlt={"Sakura"} />
@@ -35,7 +115,7 @@ function App() {
           <CardItem img={Choji} imgAlt={"Choji"} />
           <CardItem img={Hinata} imgAlt={"Hinata"} />
           <CardItem img={Shikamaru} imgAlt={"Shikamaru"} />
-          <CardItem img={Gaara} imgAlt={"Gaara"} />
+          <CardItem img={Gaara} imgAlt={"Gaara"} /> */}
         </div>
       </main>
     </>
@@ -43,3 +123,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/CardItem/CardItem.js
+++ src/components/CardItem/CardItem.js
@@ -2,10 +2,10 @@ import "./cardItem.css";
 
 const CardItem = (props) => {
     return (
-        <figure className="card-item">
-            <img src={props.img} alt={props.imgAlt} />
-            <figcaption>{props.imgAlt}</figcaption>
-        </figure>
+      <figure className="card-item" onClick={props.onClick}>
+        <img src={props.img} alt={props.alt} />
+        <figcaption>{props.alt}</figcaption>
+      </figure>
     );
 }
 
